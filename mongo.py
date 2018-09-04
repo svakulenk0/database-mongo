6148784455d4acd1074d4877791401b1dfd15665
vendi <svitlana.vakulenko@gmail.com>
2018-09-04 10:09:50 +0200

    chg: query

--- mongo.py
+++ mongo.py
@@ -37,11 +37,7 @@ class DatabaseMongo(Database):
     async def get(self, key):
         """Get a document from the given mongo collection."""
         # hack parsing a single string key into the mongo collection name and search query parameters
-        collection, field, value = key.split('/')
-        print (collection)
-        print (field)
-        print (value)
         logging.debug("Getting the last inserted document from the " + collection + " mongo collection")
-        return await self.db[collection].find_one(
-                        {"$query": {}, "$orderby": {"$natural" : -1}}
+        return await self.db[key].find_one(
+                        {"$query": {field: value}, "$orderby": {"$natural" : -1}}
                         )
